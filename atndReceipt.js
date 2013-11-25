0b76b3a5ede9042948e05d18900ef7ec7dc4e5a3
crossnode <kojimack@gmail.com>
2013-11-26 00:05:48 +0900

    Update atndReceipt.js

--- atndReceipt.js
+++ atndReceipt.js
@@ -1,1 +1,13 @@
-$(function(){$("body").html($("#events-members").html());$(".a-b li em").each(function(){$(this).remove();});$(".a-b li").each(function(){$(this).prepend('<input type="checkbox"> <input type="checkbox">');});$(".a-b li").css("list-style-position","inside");$(".a-b li a").removeAttr("href").css("text-decoration","none");$("*").css("text-align","left");$("head").append('<meta name="viewport" content="width=device-width">');$("#members-info,#members-cancel").remove();});
+$(function(){
+  $("body").html($("#events-members").html(););
+  $(".a-b li em").each(function(){
+    $(this).remove();
+  });
+  $(".a-b li").css("list-style-position","inside").each(function(){
+    $(this).prepend('<input type="checkbox">/<input type="checkbox">');
+  });
+  $(".a-b li a").removeAttr("href").css("text-decoration","none");
+  $("*").css("text-align","left");
+  $("head").append('<meta name="viewport" content="width=device-width">');
+  $("#members-info,#members-cancel").remove();
+});
